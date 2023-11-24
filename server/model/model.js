154d9a4e3e82e3b7d84bf086f113ab9af68eb36d
John <johnnoh0719@gmail.com>
2023-11-24 17:58:55 -0600

    updated with model

--- server/model/model.js
+++ server/model/model.js
@@ -1,1 +1,45 @@
-// mongo model
+const mongoose = require('moongse');
+const Schema = mongoose.Schema;
+
+const petSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+
+	hunger: {
+		type: Number,
+		required: true,
+		default: 50,
+	},
+
+	thirst: {
+		type: Number,
+		required: true,
+		default: 50,
+	},
+
+	happiness: {
+		type: Number,
+	},
+
+	life: {
+		type: Boolean,
+		required: true,
+		default: true,
+	},
+
+	age: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+
+	picture: {
+		type: Number,
+		required: true,
+	},
+});
+
+const Pet = mongoose.model('pets', petSchema);
+module.exports = Pet;
