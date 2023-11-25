be903cce8a510ce5206e351b7689dd6692a3db1c
John <johnnoh0719@gmail.com>
2023-11-24 18:59:55 -0600

    server updated

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,1 +1,20 @@
-// controller to send back to api 
+const Pet = require('../model/model');
+
+const petController = {};
+
+// retrieve all pets information when we go to /pets
+
+petController.getPets = (req, res, next) => {
+	Pet.find({})
+		.then((pets) => {
+			res.locals.getPets = pets;
+			console.log(res.locals.getPets);
+			return next();
+		})
+		.catch((error) => {
+			console.log('Error in getting pets', error);
+			return res.status(500).json({ error: 'Internal Server Error' });
+		});
+};
+
+module.exports = petController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,1 +1,10 @@
-// routes
+const express = require('express');
+const petController = require('../controller/controller');
+const router = express.Router();
+
+router.get('/pets', petController.getPets, (req, res) => {
+	console.log('made it to api', res.locals.getPets);
+	return res.status(200).json(res.locals.getPets);
+});
+
+module.exports = router;
