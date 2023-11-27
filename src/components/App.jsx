83bbcb1919cf9948eaebdf09d7febe26f656b1f1
Joshua McDaniel <joshuamcdaniel@Joshys-MacBook-Pro.local>
2023-11-27 12:54:06 -0700

    Finished most of dropdown menu functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import CreatePage from './CreatePage.jsx';
-import { BrowserRouter as Router, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PetPage from './PetPage.jsx';
 
-function App(){
-    return(
+function App() {
+    return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route exact path='/' Component={CreatePage} />
                 <Route exact path='/petpage' Component={PetPage} />
                 {/* <Route exact path='/login' Component={LoginPage} */}
-            </Switch>
+            </Routes>
         </Router>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,79 +7,79 @@ import crab from '../assets/yeti-crab-prints Medium.jpeg'
 
 //    event.preventDefault();
 function Form() {
-    const navigate = useNavigate();
-    function handleContinue (e) {
-        //get input from name input
-        e.preventDefault();
-        const inputName = document.getElementById('name').value;
-        console.log('inputName: ', inputName);
-        let petPicture;
-        //get input from whichever picture is chosen
-        petPicture = document.querySelector('input[name="petName"]:checked').value;
-        console.log('petPicture: ', petPicture);
-        //send this data on POST request body
-        fetch('http://localhost:3000/create', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({name: inputName, picture: petPicture})
+  const navigate = useNavigate();
+  function handleContinue(e) {
+    //get input from name input
+    e.preventDefault();
+    const inputName = document.getElementById('name').value;
+    console.log('inputName: ', inputName);
+    let petPicture;
+    //get input from whichever picture is chosen
+    petPicture = document.querySelector('input[name="petName"]:checked').value;
+    console.log('petPicture: ', petPicture);
+    //send this data on POST request body
+    fetch('http://localhost:3000/create', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ name: inputName, picture: petPicture })
 
-        })
-        //THEN invoke react router for PetPage
-        .then(() => {
-          navigate('/petpage')
-        })
-            // .then(() => {
-            //     fetch('http://localhost:8080/create/pets', {
-            //         method: 'GET',
-            //         mode: 'cors'
-            //     })
-            // })
-            .catch((error) => console.log('this is not working', error))
-    }
+    })
+      //THEN invoke react router for PetPage
+      .then(() => {
+        navigate('/petpage')
+      })
+      // .then(() => {
+      //     fetch('http://localhost:8080/create/pets', {
+      //         method: 'GET',
+      //         mode: 'cors'
+      //     })
+      // })
+      .catch((error) => console.log('this is not working', error))
+  }
 
-    return(
-   <form id="create pet">
+  return (
+    <form id="create pet">
 
-        <label htmlFor="name">Pet's name</label>
-        <input type="text" id="name"/>
+      <label htmlFor="name">Pet's name</label>
+      <input type="text" id="name" />
 
       {/* <fieldset> */}
+      <picture>
+        {/* <source srcSet="https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg" /> */}
+        <img src={shark} alt="shark" style={{ width: '48px', height: '48px' }} />
+      </picture>
+      <label htmlFor="pet1">Pet 1</label>
+      <input type="radio" id="shark" name="petName" value="shark" />
+
+      <div id='pet-box'>
         <picture>
-            {/* <source srcSet="https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg" /> */}
-            <img src={shark} alt="shark" style={{width: '48px', height: '48px'}}/>
+          {/* <source srcSet='https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg' /> */}
+          <img
+            src={redlip}
+            alt="red-lip"
+            style={{ width: '48px', height: '48px' }}
+          />
         </picture>
-        <label htmlFor="pet1">Pet 1</label>
-        <input type="radio" id="shark" name="petName" value="shark"/>
+        <label htmlFor='pet2'>Pet 2</label>
+        <input type='radio' id='batfish' name='petName' value='batfish' />
+      </div>
 
-        <div id='pet-box'>
-          <picture>
-            {/* <source srcSet='https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg' /> */}
-            <img
-              src={redlip}
-              alt="red-lip"
-              style={{ width: '48px', height: '48px' }}
-            />
-          </picture>
-          <label htmlFor='pet2'>Pet 2</label>
-          <input type='radio' id='batfish' name='petName' value='batfish' />
-        </div>
-
-        <div id='pet-box'>
-          <picture>
-            {/* <source srcSet='https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg' /> */}
-            <img
-              src={crab}
-              alt='shark'
-              style={{ width: '48px', height: '48px' }}
-            />
-          </picture>
-          <label htmlFor='pet3'>Pet 3</label>
-          <input type='radio' id='crab' name='petName' value='crab' />
-        </div>
-      </fieldset>
+      <div id='pet-box'>
+        <picture>
+          {/* <source srcSet='https://cdn3.vectorstock.com/i/1000x1000/08/17/goblin-shark-vector-9340817.jpg' /> */}
+          <img
+            src={crab}
+            alt='shark'
+            style={{ width: '48px', height: '48px' }}
+          />
+        </picture>
+        <label htmlFor='pet3'>Pet 3</label>
+        <input type='radio' id='crab' name='petName' value='crab' />
+      </div>
+      {/* </fieldset> */}
 
       <button onClick={handleContinue}>Continue</button>
     </form>
