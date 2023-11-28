560691df15ae0b22def6519cb38daa20f48bbcfd
Michelle <mxie97@gmail.com>
2023-11-28 12:24:20 -0800

    initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,74 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports= {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    host: 'localhost',
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
     },
-    devServer: {
-        static: {
-            publicPath: '/dist',
-            directory: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: { '/**': { target: 'http://localhost:3000', secure: false } },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-        port: 8080,
-        // proxy: {
-        //     '/create': {
-        //         target: 'http://localhost:8080',
-        //         router: () => 'http://localhost:3000',
-        //         logLevel: 'debug',
-        //     }
-        // }
-        proxy: { '/create': 'http://localhost:3000'}
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html',
-        })
+      },
+      {
+        test: /\.scss/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
     ],
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'fonts/',
-						},
-					},
-				],
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-                include: /assets/,
-				use: [
-					{
-						loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                            publicPath: 'images/'
-                        }
-					},
-				],
-			},
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss'],
-    }
-}
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+  },
+};
